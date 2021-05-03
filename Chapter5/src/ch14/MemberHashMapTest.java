baf03561dbc9ca7ea816e95097aeb62d6486a168
Si-Hyeak-KANG <79829085+Si-Hyeak-KANG@users.noreply.github.com>
2021-05-03 16:14:17 +0900

    Map 인터페이스 구현 <HashMap>

---
+++ Chapter5/src/ch14/MemberHashMapTest.java
@@ -0,0 +1,33 @@
+package ch14;
+
+import java.util.HashMap;
+
+public class MemberHashMapTest {
+
+	public static void main(String[] args) {
+		
+		MemberHashMap memberHashMap = new MemberHashMap();
+		
+		Member memberLee = new Member(1001, "이순신");
+		Member memberKim = new Member(1002, "김유신");
+		Member memberKang = new Member(1003, "강감찬");
+		Member memberHong = new Member(1004,"홍길동");
+		
+
+		memberHashMap.addMember(memberLee);
+		memberHashMap.addMember(memberKim);
+		memberHashMap.addMember(memberKang);
+		memberHashMap.addMember(memberHong);
+		
+		memberHashMap.showAllMember();
+		
+		HashMap<Integer, String> hashMap = new HashMap<Integer,String>();
+		hashMap.put(1001,"Kim");
+		hashMap.put(1002,"Lee");
+		hashMap.put(1003,"Kang");
+		hashMap.put(1004,"Hong");
+		
+		System.out.println(hashMap); //원래 HashMap의 toString -> key=value 호출 쌍으로 저장
+	}
+
+}
