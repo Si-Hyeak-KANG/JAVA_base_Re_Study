00a4ae6551fc0bd105db3937f22b4e74732162a6
Si-Hyeak-KANG <79829085+Si-Hyeak-KANG@users.noreply.github.com>
2021-05-06 19:43:20 +0900

    Stream

---
+++ Chapter6/src/ch06/ArrayListStreamTest.java
@@ -0,0 +1,29 @@
+package ch06;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+
+
+public class ArrayListStreamTest {
+
+	public static void main(String[] args) {
+
+		List<String> sList = new ArrayList<String>();
+		sList.add("Tomas"); //5
+		sList.add("Edward"); //6
+		sList.add("Jack"); //4
+		
+		//컬렉션에 대한 Stream을 사용할 때는 제너릭 타입으로 가지고 있는 것을 명시
+		Stream<String> stream = sList.stream();
+		stream.forEach(s->System.out.println(s));
+		
+		sList.stream().sorted().forEach(s->System.out.print(s + "\t"));
+		System.out.println();
+		sList.stream().map(s->s.length()).forEach(n->System.out.print(n + "\t"));
+		System.out.println();
+		sList.stream().filter(s->s.length() >= 5).forEach(s->System.out.print(s + "\t"));
+	}
+
+}
---
+++ Chapter6/src/ch06/IntArrayStreamTest.java
@@ -0,0 +1,29 @@
+package ch06;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class IntArrayStreamTest {
+
+	public static void main(String[] args) {
+
+		int[] arr = {1,2,3,4,5};
+		
+		for(int num : arr) {
+			System.out.println(num);
+		}
+		
+		System.out.println();
+		
+		IntStream is = Arrays.stream(arr); //얘가 반환해주는 애가 IntStream
+		//모든 array들은 arrays 클래스를 활용 가능
+		//forEach는 element를 하나씩 꺼내라
+		is.forEach(n->System.out.println(n)); //위와 동일
+		//한번 연산이 시행이되고나면 소멸함.
+		//stream 메서드는 매번 재생성 후 호출해야 한다.
+		int sum = Arrays.stream(arr).sum();
+		System.out.println(sum);
+	}
+
+} 
+  
