bd941953215f29a0e397efcf4da2b8f890fa5c71
Si-Hyeak-KANG <79829085+Si-Hyeak-KANG@users.noreply.github.com>
2021-05-16 23:48:22 +0900

    project- 성적 관리 프로그램(1)

---
+++ Chapter7/src/school/School.java
@@ -0,0 +1,47 @@
+package school;
+
+import java.util.ArrayList;
+
+//ÇÐ±³´Â ½Ì±ÛÅæ ÆÐÅÏ
+public class School {
+
+	private static School instance = new School();
+	
+	private ArrayList<Student> studentList = new ArrayList<Student>();
+	private ArrayList<Subject> subjectList = new ArrayList<Subject>();
+	
+	private School() {}
+	
+	public static School getInstance() {
+		
+		if( instance == null) 
+			instance = new School();
+		
+		return instance;
+	}
+
+	public ArrayList<Student> getStudentList() {
+		return studentList;
+	}
+
+	public void setStudentList(ArrayList<Student> studentList) {
+		this.studentList = studentList;
+	}
+
+	public ArrayList<Subject> getSubjectList() {
+		return subjectList;
+	}
+
+	public void setSubjectList(ArrayList<Subject> subjectList) {
+		this.subjectList = subjectList;
+	}
+	
+	public void addStudent(Student student) {
+		studentList.add(student);
+		
+	}
+	
+	public void addSubject(Subject subject) {
+		subjectList.add(subject);
+	}
+}
---
+++ Chapter7/src/test/TestMain.java
@@ -0,0 +1,22 @@
+package test;
+
+import school.School;
+import school.Subject;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+
+		School goodSchool = School.getInstance();
+		Subject korean;
+		Subject math;
+		
+		GenerateGradeReport gradeReport = new GenerateGradeReport();
+		
+		public static void main(String[] args) {
+			
+			
+		}
+	} 
+
+}
